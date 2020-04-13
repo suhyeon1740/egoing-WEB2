ab0670674af556b6e1c39295fa4a81a93329f1ef
Elly (박수현) <elly@treenod.com>
2020-04-13 18:26:31 +0900

    URL을 통해서 입력된 값 사용하기

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function (request, response) {
-    var url = request.url;
-    if (request.url == '/') {
-        url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query; //querystring 가져오기
+    console.log(queryData.id)
+    if (_url == '/') {
+        _url = '/index.html';
     }
-    if (request.url == '/favicon.ico') {
+    if (url == '/favicon.ico') {
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    // console.log(__dirname + url)
+    // __dirname: main.js 파일이 있는 디렉토리 위치
+    // url: 클라이언트가 요청한 url
+    response.end(queryData.id);
 
 });
 app.listen(3000);
