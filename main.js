af115d91bf91d86ec63c5c1644348682f80892ad
Elly (박수현) <elly@treenod.com>
2020-04-16 19:09:17 +0900

    홈페이지 구현

--- main.js
+++ main.js
@@ -4,35 +4,65 @@ var url = require('url');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
-    var queryData = url.parse(_url, true).query; //querystring 가져오기
-    var title = queryData.id   
+    var queryData = url.parse(_url, true).query; //querystring 가져오기    
     var pathname = url.parse(_url).pathname //url.parse()메소드는 URL 문자열을 가져와 구문 분석 한 후 URL 객체를 반환합니다.
     if ( pathname === '/') {
-        fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-            <title>WEB1 - HTML</title>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1><a href="/">WEB - ${title}</a></h1>
-            <ol>
-                <li><a href="?id=HTML">HTML</a></li>
-                <li><a href="?id=CSS">CSS</a></li>
-                <li><a href="?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>
-                ${description}
-            </p>
-            </body>
-            </html>
-            `
-            response.writeHead(200); // 200: 성공 , 404: 찾을 수 없음
-            response.end(template);
-        })
+        if (queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+                var title = 'Welcome'
+                var description = 'Heelo, Node.js'
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - HTML</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB - ${title}</a></h1>
+                <ol>
+                    <li><a href="?id=HTML">HTML</a></li>
+                    <li><a href="?id=CSS">CSS</a></li>
+                    <li><a href="?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>
+                    ${description}
+                </p>
+                </body>
+                </html>
+                `
+                response.writeHead(200); // 200: 성공 , 404: 찾을 수 없음
+                response.end(template);
+            })
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+                var title = queryData.id   
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - HTML</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB - ${title}</a></h1>
+                <ol>
+                    <li><a href="?id=HTML">HTML</a></li>
+                    <li><a href="?id=CSS">CSS</a></li>
+                    <li><a href="?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>
+                    ${description}
+                </p>
+                </body>
+                </html>
+                `
+                response.writeHead(200); // 200: 성공 , 404: 찾을 수 없음
+                response.end(template);
+            })
+        }        
     } else {
         response.writeHead(404)
         response.end("Not found")
