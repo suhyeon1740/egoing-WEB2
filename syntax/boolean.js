f87ceec2261992015ac19e676a2e5a31fa3b714e
Elly (박수현) <elly@treenod.com>
2020-04-16 18:16:39 +0900

    Not found 구현

---
+++ syntax/boolean.js
@@ -0,0 +1,5 @@
+console.log(true)
+console.log(false)
+true = 1 // 에러발생
+// name = 1 대입연산자
+// name == 1 비교연산자
---
+++ syntax/if.js
@@ -0,0 +1,15 @@
+var args = process.argv
+console.log('A')
+console.log('B')
+if(args[2] === '1') {
+    console.log('C1')
+} else {
+    console.log('C2')
+}
+console.log('D')
+
+//# parameter (매개변수)
+//# 함수의 정의 부분에 나열되어 있는 변수
+
+//# argument(전달인자)
+//# 함수를 호출할때 전달 되는 실제 값,
