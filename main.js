323924c6f22484f45b8b963bb878075e7fa8ceff
elly <suhyeon1740@gmail.com>
2020-04-19 19:13:57 +0900

    Node.js에서 파일목록 알아내기

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var app = http.createServer(function (request, response) {
         if (queryData.id === undefined) {
             fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
                 var title = 'Welcome'
-                var description = 'Heelo, Node.js'
+                var description = 'Hello, Node.js'
                 var template = `
                 <!doctype html>
                 <html>
---
+++ syntax/array-loop.js
@@ -0,0 +1,8 @@
+var num = [3, 100, 299, 1002]
+var i = 0
+var total = 0
+while (i < num.length) {
+    total += num[i]
+    i++
+}
+console.log(total)
