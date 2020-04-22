1257271747df4347158b2fd0e5751ef90f4207a2
Elly (박수현) <elly@treenod.com>
2020-04-22 12:34:43 +0900

    파일명 변경, 내용 저장

--- main.js
+++ main.js
@@ -113,6 +113,24 @@ var app = http.createServer(function (request, response) {
                 response.end(template);
             })            
         })
+    } else if( pathname === '/update_process') {
+        var body = '';        
+        request.on('data', function (data) {
+            body += data;
+        })        
+        request.on('end', function () {
+            var post = qs.parse(body)
+            var id = post.id
+            var title = post.title
+            var description = post.description
+            fs.rename(`data/${id}`, `data/${title}`, function() {
+                fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                    // 리다이랙션: 사용자를 다른 주소로 보내버림
+                    response.writeHead(302, { Location: `/?id=${title}` });
+                    response.end();
+                })
+            })            
+        })   
     } else {
         response.writeHead(404)
         response.end("Not found")
