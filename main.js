de3c1cf5feb3d868a7c4ae457a0d5866f7ec9058
Elly (박수현) <elly@treenod.com>
2020-04-22 13:23:42 +0900

    글삭제 기능 완성

--- main.js
+++ main.js
@@ -136,6 +136,19 @@ var app = http.createServer(function (request, response) {
                 })
             })            
         })   
+    } else if (pathname === '/delete_process') {
+        var body = '';        
+        request.on('data', function (data) {
+            body += data;
+        })        
+        request.on('end', function () {
+            var post = qs.parse(body)
+            var id = post.id
+            fs.unlink(`data/${id}`, function(err) {
+                response.writeHead(302, { Location: `/` });
+                response.end();
+            })        
+        }) 
     } else {
         response.writeHead(404)
         response.end("Not found")
