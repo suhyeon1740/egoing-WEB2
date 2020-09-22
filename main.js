acd995593c6c908cbd1fe172a0d0eab34a4e8e6b
elly <suhyeon1740@gmail.com>
2020-09-22 18:12:43 +0900

    인증구현 - 접근제어

--- main.js
+++ main.js
@@ -3,8 +3,29 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring')
 var template = require('./lib/template.js')
+const cookie = require("cookie")
 // request : 요청과 관련된 정보
 // response: 응답과 관련된 정보
+
+function authIsOwner(req, res) {
+    let isOwner = false
+    let cookies = {}
+    if(req.headers.cookie) {
+        cookies = cookie.parse(req.headers.cookie)
+    }
+    if (cookies.email == "admin@gmail.com" && cookies.password == "1111") {
+        isOwner = true
+    }
+    return isOwner
+}
+
+function setLoginStatus(request, response) {
+    if (authIsOwner(request, response)) {
+        return `<a href="/logout_process">Logout</a>`
+    } else {
+        return `<a href="/login">Login</a>`
+    }
+}
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query; //querystring 가져오기    
@@ -14,7 +35,7 @@ var app = http.createServer(function (request, response) {
             fs.readdir('./data', function (error, fileList) {
                 var title = 'Welcome'
                 var description = 'Hello, Node.js'
-                var html = template.html(title, fileList, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">create</a>`)
+                var html = template.html(title, fileList, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">create</a>`, setLoginStatus(request, response))
                 response.writeHead(200); // 200: 성공 , 404: 찾을 수 없음
                 response.end(html); 
             })            
@@ -24,7 +45,8 @@ var app = http.createServer(function (request, response) {
                     var title = queryData.id   
                     var form = template.form('/delete_process', title)
                     var html = template.html(title, fileList, `<h2>${title}</h2><p>${description}</p>${form}`, 
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`)
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+                        setLoginStatus(request, response))
                     response.writeHead(200); // 200: 성공 , 404: 찾을 수 없음
                     response.end(html);
                 })
@@ -34,11 +56,15 @@ var app = http.createServer(function (request, response) {
         fs.readdir('./data', function (error, fileList) {
             var title = 'WEB - create'
             var form = template.form('/process_create')
-            var html = template.html(title, fileList, form, '')
+            var html = template.html(title, fileList, form, '', setLoginStatus(request, response))
             response.writeHead(200);
             response.end(html);
         })         
     } else if ( pathname === '/process_create') {
+        if (!authIsOwner(request, response)) {
+            response.end('Login Required!')
+            // return false;
+        }
         var body = '';
         // post 데이터가 너무 많은걸 대비해 data를 조각조각 나눠서 받음
         // data를 받을때마다 아래처럼 body에 데이터를 추가해줌
@@ -61,7 +87,8 @@ var app = http.createServer(function (request, response) {
             fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
                 var title = queryData.id
                 var form = template.form('/update_process', title, description)
-                var html = template.html(title, fileList, form, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`)
+                var html = template.html(title, fileList, form, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+                    setLoginStatus(request, response))
                 response.writeHead(200);
                 response.end(html);
             })            
@@ -109,7 +136,8 @@ var app = http.createServer(function (request, response) {
                 <p><input type="password" name="password" placeholder="password" /></p>
                 <p><input type="submit" /></p>
                 </form>`,
-                `<a href="/create">create</a>`
+                `<a href="/create">create</a>`,
+                setLoginStatus(request, response)
             )
             response.writeHead(200)
             response.end(html)
@@ -135,6 +163,23 @@ var app = http.createServer(function (request, response) {
             } else {
                 response.end("??")
             }
+        })
+    } else if (pathname === "/logout_process") {
+        var body = ""
+        request.on("data", function (data) {
+            body = body + data
+        })
+        request.on("end", function () {
+            const post = qs.parse(body)
+            response.writeHead(302, {
+                "Set-Cookie": [
+                    `email=; Max-age=0`,
+                    `password=; Max-age=0`,
+                    `nickname=; Max-age=0`,
+                ],
+                Location: "/",
+            })
+            response.end()            
         })
     } else {
         response.writeHead(404)
